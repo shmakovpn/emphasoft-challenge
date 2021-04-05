c0bb280d106d8cc25b4d3133c1bc9d7dd26a3e28
shmakovpn <shmakovpn@yandex.ru>
2021-04-05 07:38:29 +0300

    task1 finished

--- task1.py
+++ task1.py
@@ -15,14 +15,26 @@ import hashlib
 import os
 from datetime import datetime
 import json
+from PyPDF2 import PdfFileReader
+from io import BytesIO
+import warnings
+
 
 FetchFuncStr = Coroutine[Any, Any, str]
 FetchFuncListStr = Coroutine[Any, Any, List[str]]
+FetchFuncListBytes = Coroutine[Any, Any, List[bytes]]
 SITE_URL: str = 'https://www.irs.gov/'
-ROOT_SITEMAP_URL: str = f'{SITE_URL}/sitemap.xml'
+ROOT_SITEMAP_URL: str = f'{SITE_URL}sitemap.xml'
 SCRIPT_DIR: str = os.path.dirname(os.path.abspath(__file__))
 CACHE_DIR: str = os.path.join(SCRIPT_DIR, 'cache')
 current_year: int = datetime.now().year
+# proxy configuration
+try:
+    from proxy import proxy_args
+except ImportError:
+    proxy_args: Dict[str, Any] = {}
+# task1 data
+from task1_data import task1_data
 
 
 class Page():
@@ -49,7 +61,7 @@ class Page():
     async def _fetch_from_internet(self) -> str:
         response: ClientResponse
         try:
-            async with self.session.get(self.url) as response:
+            async with self.session.get(self.url, **proxy_args) as response:
                 return await response.text()
         except ClientConnectionError as e:
             print(f'Error: fetching url="{self.url}" was failed: {e}')
@@ -80,7 +92,7 @@ class Sitemap(Page):
     def __init__(self, url: str) -> None:
         super().__init__(url)
         self.sitemap_urls = []
-    
+
     async def urls(self) -> List[str]:
         if self.sitemap_urls:
             return self.sitemap_urls
@@ -97,10 +109,51 @@ class RootSitemap(Sitemap):
         super().__init__(url)
 
 
+class PdfPage(Page):
+    _content: bytes = ''
+
+    def _cache_filename(self) -> str:
+        return os.path.join(CACHE_DIR, self.url.split('/').pop())
+
+    async def _fetch_from_cache(self) -> bytes:
+        cache_filename: str = self._cache_filename()
+        if os.path.isfile(cache_filename):
+            async with aiofiles.open(cache_filename, mode='rb') as f:
+                return await f.read()
+        return bytes()  # return an empty string if cache does not exist
+
+    async def _fetch_from_internet(self) -> bytes:
+        response: ClientResponse
+        try:
+            async with self.session.get(self.url, **proxy_args) as response:
+                return await response.content.read()
+        except ClientConnectionError as e:
+            print(f'Error: fetching url="{self.url}" was failed: {e}')
+            return bytes()  # return an empty string if an error has occurred
+
+    async def _save_to_cache(self, content: bytes) -> None:
+        cache_filename: str = self._cache_filename()
+        async with aiofiles.open(cache_filename, mode='wb') as f:
+            await f.write(content)
+
+    async def _fetch(self) -> bytes:
+        cache_content: str = await self._fetch_from_cache()
+        if cache_content:
+            return cache_content
+        return await self._fetch_from_internet()
+
+    async def content(self) -> bytes:
+        if self._content:
+            return self._content
+        self._content = await self._fetch()
+        await self._save_to_cache(self._content)
+        return self._content
+
 def url_filter(url: str) -> bool:
     if not url.endswith('.pdf'):
         return False
-    if url.startswith('https://www.irs.gov/pub/irs-prior/f') or url.startswith('https://www.irs.gov/pub/irs-pdf/f'):
+    if url.startswith('https://www.irs.gov/pub/irs-prior/f') or url.startswith(
+            'https://www.irs.gov/pub/irs-pdf/f'):
         return True
     return False
 
@@ -109,17 +162,17 @@ class Form:
     min_year: int = current_year
     max_year: int = 0
     code: str = ''
-    
+
     @staticmethod
     def parse_url(url: str) -> List[str]:
         url_parts: List[str] = url.split('/')
         filename: str = url_parts.pop()[1:].rsplit('.', 1)[0]
         return filename.split('--')
-    
+
     def __init__(self, code_year: List[str]):
         self.code = code_year[0]
         self.update(code_year)
-    
+
     def update(self, code_year: List[str]) -> None:
         year: int = current_year
         if len(code_year) > 1:
@@ -128,16 +181,22 @@ class Form:
             self.max_year = year
         if self.min_year > year:
             self.min_year = year
-    
+
     def to_dict(self) -> Dict[str, Any]:
         return {
             'code': self.code,
             'min_year': self.min_year,
             'max_year': self.max_year,
+            'url': self.url(),  # debug
         }
-    
+
     def __str__(self) -> str:
         return json.dumps(self.to_dict())
+
+    def url(self) -> str:
+        if self.max_year == current_year:
+            return f'https://www.irs.gov/pub/irs-pdf/f{self.code}.pdf'
+        return f'https://www.irs.gov/pub/irs-prior/f{self.code}--{self.max_year}.pdf'
 
 
 class FormJson(Form):
@@ -145,7 +204,7 @@ class FormJson(Form):
         self.min_year = form_json['min_year']
         self.max_year = form_json['max_year']
         self.code = form_json['code']
-    
+
 
 async def get_urls_filtered() -> List[str]:
     urls_filtered: List[str]
@@ -157,6 +216,7 @@ async def get_urls_filtered() -> List[str]:
         async with ClientSession() as Page.session:
             root_sitemap: RootSitemap = RootSitemap()
             sitemap_urls: List[str] = await root_sitemap.urls()
+            assert sitemap_urls  # be sure that root sitemap loaded correctly
             sitemaps: List[Sitemap] = []
             tasks: List[FetchFuncListStr] = []
             for sitemap_url in sitemap_urls:
@@ -198,10 +258,47 @@ async def get_forms() -> Dict[str, Form]:
     return forms
 
 
+def parse_task_code(task_code: str) -> str:
+    return task_code.lower().replace('form', '').strip().replace('-', '')
+
+
 async def main():
     forms: Dict[str, Form] = await get_forms()
-    print(len(forms))
-        
+    for task_code in task1_data:
+        parsed_task_code: str = parse_task_code(task_code)
+        if parsed_task_code not in forms:
+            print(
+                json.dumps({
+                    'error':
+                    True,
+                    'message':
+                    f'task code "{parsed_task_code}" does not exist',
+                }))
+        else:
+            async with ClientSession() as PdfPage.session:
+                pdf_page: PdfPage = PdfPage(forms[parsed_task_code].url())
+                content: bytes = await pdf_page.content()
+                form_dict: Dict[str, Any] = forms[parsed_task_code].to_dict()
+                form_dict['form_title'] = ''
+                if not content:
+                    form_dict['error_message'] = 'could not fetch PDF content'
+                    print(json.dumps(form_dict))
+                else:
+                    try: 
+                        with warnings.catch_warnings():
+                            warnings.simplefilter("ignore")
+                            pdf = PdfFileReader(BytesIO(content))
+                            info = pdf.getDocumentInfo()
+                            try:
+                                subject: str = info['/Subject']
+                                form_dict['form_title'] = subject
+                            except:
+                                form_dict['error_message'] = 'PDF error, /Subject does not exist'
+                    except:
+                        form_dict['error_message'] = 'PDF reading error'
+                _ = form_dict.pop('code')
+                _ = form_dict.pop('url')
+                print(json.dumps(form_dict))  # PRINT RESULT
 
 
 if __name__ == '__main__':
---
+++ task1_data.py
@@ -0,0 +1,15 @@
+"""
+Data for task 1
+
+Author: shmakovpn <shmakovpn@yandex.ru>
+Date: 2021-04-5
+"""
+from typing import List
+
+task1_data: List[str] = [
+    'Form W-2',  # value from the task
+    'Form 1095-C',  # value from the task
+    '1120nd',  # value that cannot be find using the site search form
+    '5500',  # 1975-2008
+    'foo_bar',  # error value
+]
